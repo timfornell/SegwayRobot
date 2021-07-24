97e0c28ae48ed9587b2ac2feb044aa69ea48220a
Vincent <tim.fornell@hotmail.com>
2021-07-24 18:12:45 +0200

    Integrate EKF into other code

---
+++ Code/main/CommonDefinitions.hpp
@@ -0,0 +1,6 @@
+#ifndef CommonDefinitions_hpp
+#define CommonDefinitions_hpp
+
+#define dT (11e-3) // 11 milliseconds
+
+#endif
--- Code/main/MotorController.hpp
+++ Code/main/MotorController.hpp
@@ -8,10 +8,6 @@
 #include "CommandHandling.hpp"
 
 /* Definitions */
-#define BUFFER_SIZE (10)
-#define ERROR_BUFFER_SIZE (1)
-#define dT (11e-6) // 10 microseconds
-
 struct ControllerSavedValues
 {
     float integralValue;
